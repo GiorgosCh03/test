422c4c454395626e6a0468b5d585c9779fdc502f
George Chasapis <georgexasapis2003@gmail.com>
2024-07-19 16:31:10 +0300

    first commit

---
+++ test/script.js
@@ -0,0 +1,8 @@
+function button(){
+
+    const paragraph1 = document.getElementById('button');
+     if(paragraph1.style_color == '#000' )
+paragraph1.style_color= '#ff000';
+     else
+     paragraph1.style_color= '#000';
+}
